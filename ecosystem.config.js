ebaebac61ffa2f71f2639a9165a99f77fc895626
Alex Stupak <stupak.alexn@gmail.com>
2022-01-17 17:18:03 +0200

    fix path for ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
 			name: 'logger',
 			script: `yarn start:prod`,
 			cwd: `${currentPath}/gsp-logger`,
-			exec_interpreter: "/root/.nvm/versions/node/v10.17.0/bin/node",
+			exec_interpreter: "/home/stupak.alexn/.nvm/versions/node/v10.17.0/bin/node",
 			env: {
 				NODE_ENV: 'production',
 			}
